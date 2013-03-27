f2e6a6824f0a362eefbe4e1add325b99cc3068f6
DarkCthulhu <anirudh4444@gmail.com>
2013-03-27 23:38:06 +0530

    tk-ui works with sqlite database

--- pynote/interface.py
+++ pynote/interface.py
@@ -1,27 +1,54 @@
 # -*- coding: utf-8 -*-
 from Tkinter import * 
-#from sqlite.sqlitehelper import Sqlitehelper
-
-root = Tk() 
-root.title("TkInter")
-
-button1 = Button(root, text="button1", command=Button1) 
-button2 = Button(root, text="button2") 
-button3 = Button(root, text="button3", command=Button3)  
-text = Entry(root) 
-listbox = Listbox(root) 
-scrollbar = Scrollbar(root, orient=VERTICAL) 
-listbox = Listbox(root, yscrollcommand=scrollbar.set) 
-scrollbar.configure(command=listbox.yview) 
+import tkMessageBox
+from sqlite.connections import PyNoteSqlite
 
 def main():
-    button1.pack()
-    button2.pack()
-    button3.pack()
+    #global variables to manipulate the interface
+    global root, button1, scrollbar, listbox, text
+    
+    #global variable to deal with sqlite
+    global database_handle
+    database_handle = PyNoteSqlite()
+    
+    # interface setup
+    root = Tk() 
+    root.title("TkInter")
+    button_ins = Button(root, text="Insert", command=insert_callback) 
+    button_del = Button(root, text="Delete", command=delete_callback) 
+    text = Entry(root) 
+    listbox = Listbox(root) 
+    scrollbar = Scrollbar(root, orient=VERTICAL) 
+    listbox = Listbox(root, yscrollcommand=scrollbar.set) 
+    scrollbar.configure(command=listbox.yview) 
+     
+    button_ins.pack()
+    button_del.pack()
     text.pack()
     listbox.pack()
     
+    # call to populate existing notes from database 
+    populateNotes()
+    
     root.mainloop() 
+    
+# Event handler for insert button
+def insert_callback():
+    listbox.insert(END, text.get())
+    database_handle.insert_value(["hit", text.get()])
+    text.delete(0, END)
+    #tkMessageBox.showinfo("Hit", "Bit")
+    
+# Event handler for delete button
+def delete_callback():
+    print listbox.get(listbox.curselection()),
+    database_handle.delete_value(listbox.get(listbox.curselection()))
+    listbox.delete(listbox.curselection())
+    
+def populateNotes():
+    rows = database_handle.get_all_values()
+    for row in rows:
+        listbox.insert(END, row[1])
 
 if(__name__ == '__main__'):
-    main();
+    main()
--- pynote/sqlite/connections.py
+++ pynote/sqlite/connections.py
@@ -1,12 +1,26 @@
 from sqlitehelper import SqliteHelper
 
-def main():
-    TABLE_QUERY = "CREATE TABLE if not exists snips (title text, note text)"
-    INSERT_QUERY = "INSERT INTO snips VALUES(?, ?)"
+class PyNoteSqlite():
+    DB_NAME = "test.db"
+    TABLE_NAME = "snips"
+    TABLE_QUERY = "CREATE TABLE if not exists %s (title text, note text)" % TABLE_NAME
+    INSERT_QUERY = "INSERT INTO %s VALUES(?, ?)" % TABLE_NAME
+    SELECT_QUERY = "SELECT title, note FROM %s" % TABLE_NAME
+    DELETE_QUERY = "DELETE FROM %s WHERE note = ?" % TABLE_NAME
     
-    sqlite_helper = SqliteHelper("test.db")
-    sqlite_helper.do_query(TABLE_QUERY, None)
-    sqlite_helper.do_query(INSERT_QUERY, ["val1", "val2"])
+    # create instance of sqlite helper class &  create a table
+    def __init__(self):
+        self.sqlite_helper = SqliteHelper(PyNoteSqlite.DB_NAME)
+        self.sqlite_helper.do_modification(PyNoteSqlite.TABLE_QUERY, None)
     
-if __name__ == '__main__':
-    main()
+    # inserts values supplied into table
+    def insert_value(self, args):
+        self.sqlite_helper.do_modification(PyNoteSqlite.INSERT_QUERY, args)
+    
+    # gets values from table
+    def get_all_values(self):
+        return self.sqlite_helper.do_selection(PyNoteSqlite.SELECT_QUERY)
+        
+    # delete a value from the table
+    def delete_value(self, arg):
+        self.sqlite_helper.do_modification(PyNoteSqlite.DELETE_QUERY, [arg])
